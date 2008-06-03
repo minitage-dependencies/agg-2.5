d9776224c3ec17d2f33e13d80f2283e4ab9ecb43
kiorky <devnull@localhost>
2008-06-03 15:59:57 +0000

    ready for minitage

--- hooks/libtool.py
+++ hooks/libtool.py
@@ -1,12 +1,17 @@
 import os
 def lt(options, buildout):
     """Custom pre-make hook for building libgd"""
-    import pdb;pdb.set_trace()  ## Breakpoint ##
     #relibtoolize project"
     os.system("""cd %s;
             chmod +x autogen.sh
+            sed -e "s/--ignore-deps//g" -i autogen.sh
+            sed -e "s|aclocal|aclocal -I %s|g" -i autogen.sh
               """ % (
-                  options['compile-directory']
+                  options['compile-directory'],
+                  os.path.join( 
+                      buildout['libsdl']['location'], 
+                      'share',
+                      'aclocal')
               )
              )
 
